c434d093c331653b53eaffabeb3df966711510c2
vantiger69 <vantiger817@gmail.com>
2024-11-30 12:38:01 +0300

    complete_lab

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,34 @@
 
 def greet_programmer():
     pass
+print("Hello,programmer!")
 
 def greet(name):
+    print(f"Hello,{name}!")
     pass
+greet("Van")
 
-def greet_with_default(name="programmer"):
+
+
+def greet_with_default(name="Van"):
+    print(f"Hello,{name}")
     pass
+greet_with_default()
+
+
 
 def add(num1, num2):
-    pass
+
+    return num1 + num2
+    pass 
+result = add(1,2)
+print("The sum is:",result)
+
+
 
 def halve(number):
-    pass
+    if type(number) not in [int,float]:
+        return None
+    return number / 2
+print(halve(12))
+  
